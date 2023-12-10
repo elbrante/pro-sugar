a79faba5b4eb290002da2fc935655fd6a8856548
Albert Tazetdinov <zet565758@gmail.com>
2023-12-10 10:20:51 +0300

    добавил футер в виде компонента

--- src/pages/Lyout.jsx
+++ src/pages/Lyout.jsx
@@ -1,14 +1,12 @@
 import {Outlet} from "react-router-dom";
 import cl from '../App.module.css';
+import {Footer} from "../components/Footer/Footer";
 
 export const Layout = () => {
     return (
         <main className={cl.App}>
             <Outlet/>
-            <footer className={cl.footer}>
-                <span className={cl.text}>Сделано в</span>
-                <a href={'https://thx-loyalty.com/'} className={cl.link}>Thx loyalty</a>
-            </footer>
+            <Footer/>
         </main>
     );
 };
