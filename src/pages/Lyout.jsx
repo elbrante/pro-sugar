3577b7826a5b41b7142d0e3a440b3aee3076d768
Albert Tazetdinov <zet565758@gmail.com>
2023-12-10 10:17:12 +0300

    добавил футер

--- src/pages/Lyout.jsx
+++ src/pages/Lyout.jsx
@@ -5,6 +5,10 @@ export const Layout = () => {
     return (
         <main className={cl.App}>
             <Outlet/>
+            <footer className={cl.footer}>
+                <span className={cl.text}>Сделано в</span>
+                <a href={'https://thx-loyalty.com/'} className={cl.link}>Thx loyalty</a>
+            </footer>
         </main>
     );
 };
