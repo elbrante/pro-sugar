d2d9c9fa2ea8c188c32856fd87c81ca8cbfa3116
Albert Tazetdinov <zet565758@gmail.com>
2023-12-10 10:23:32 +0300

    добавил футер в виде компонента

--- src/pages/Lyout.jsx
+++ src/pages/Lyout.jsx
@@ -1,10 +1,12 @@
 import {Outlet} from "react-router-dom";
 import cl from '../App.module.css';
+import {Footer} from "../components/Footer/Footer";
 
 export const Layout = () => {
     return (
         <main className={cl.App}>
             <Outlet/>
+            <Footer/>
         </main>
     );
 };
